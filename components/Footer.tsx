3f46fe034464a8f8b8cdbc0f26c2f52204c90328
Abdullayev2005 <108843760+Abdullayev2005@users.noreply.github.com>
2025-08-18 06:29:18 +0500

    zayra test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
                   />
                 </svg>
               </a>
-              <a
+              {/* <a
                 aria-label="LinkedIn"
                 href="https://www.linkedin.com/"
                 target="_blank"
@@ -76,10 +76,10 @@ export default function Footer() {
                     d="M6.94 6.5A1.94 1.94 0 1 1 3.06 6.5a1.94 1.94 0 0 1 3.88 0zM4 8.89h4v10.61H4zM14.6 8.78c-2.02 0-3.02 1.11-3.53 1.89V8.89H7.2V19.5h3.88v-5.7c0-1.51.67-2.41 1.94-2.41 1.17 0 1.73.83 1.73 2.41v5.7h3.88v-6.39c0-3.23-1.72-4.73-4.03-4.73z"
                   />
                 </svg>
-              </a>
+              </a> */}
               <a
                 aria-label="X (Twitter)"
-                href="https://x.com/"
+                href="https://x.com/Zayra_uz"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-flex h-9 w-9 items-center justify-center rounded-lg ring-1 ring-white/10 hover:ring-white/20 transition"
@@ -92,6 +92,22 @@ export default function Footer() {
                 </svg>
               </a>
               <a
+                aria-label="Instagram"
+                href="https://www.instagram.com/zayra.uz/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex h-9 w-9 items-center justify-center rounded-lg ring-1 ring-white/10 hover:ring-white/20 transition"
+              >
+                <svg
+                  className="h-5 w-5 text-white/80"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M7 2h10a5 5 0 015 5v10a5 5 0 01-5 5H7a5 5 0 01-5-5V7a5 5 0 015-5zm5 5a5 5 0 100 10 5 5 0 000-10zm6.5-2a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+                </svg>
+              </a>
+
+              {/* <a
                 aria-label="GitHub"
                 href="https://github.com/"
                 target="_blank"
@@ -104,7 +120,7 @@ export default function Footer() {
                     d="M12 2a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48v-1.7c-2.78.6-3.37-1.18-3.37-1.18-.45-1.14-1.1-1.45-1.1-1.45-.9-.61.07-.6.07-.6 1 .07 1.52 1.03 1.52 1.03.9 1.53 2.36 1.09 2.93.83.09-.66.35-1.1.64-1.36-2.22-.25-4.56-1.11-4.56-4.94 0-1.09.39-2 1.03-2.7-.11-.26-.45-1.29.1-2.68 0 0 .84-.27 2.75 1.03a9.49 9.49 0 0 1 5 0c1.9-1.3 2.74-1.03 2.74-1.03.56 1.39.22 2.42.11 2.68.64.7 1.03 1.61 1.03 2.7 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.86v2.76c0 .26.18.58.69.48A10 10 0 0 0 12 2z"
                   />
                 </svg>
-              </a>
+              </a> */}
             </div>
           </div>
 
